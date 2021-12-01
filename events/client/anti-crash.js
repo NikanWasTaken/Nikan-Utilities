ec5714ce56be72f9fd708ddeeb60baffa7f79978
NikanWasTaken <nikan1252sabet4801@gmail.com>
2021-12-01 10:31:29 +0330

    main

--- events/client/anti-crash.js
+++ events/client/anti-crash.js
@@ -5,7 +5,7 @@ let client = require("../../index.js")
 
 process.on("unhandledRejection", (reason, promise) => {
 
-     const channel = client.channels.cache.get("901478033628753951")
+    const channel = client.channels.cache.get("901478033628753951")
 
     const embed = new MessageEmbed()
         .setTitle(`Unhandled Rejection`)
@@ -16,9 +16,9 @@ process.on("unhandledRejection", (reason, promise) => {
         .setFooter(`${client.user.username} Error Handling`, client.user.displayAvatarURL({ dynamic: true }))
         .setColor(`${client.embedColor.noColor}`)
 
-        console.log(reason)
-        return channel.send({ embeds: [embed]})
-        
+    console.log(reason)
+    return channel.send({ embeds: [embed] })
+
 
 });
 
@@ -28,15 +28,15 @@ process.on("uncaughtException", (err, origin) => {
     const channel = client.channels.cache.get("901478033628753951")
 
     const embed = new MessageEmbed()
-    .setTitle(`Uncaught Exception`)
-    .setURL("https://nodejs.org/api/process.html#event-uncaughtexception")
-    .addField("Error", `\`\`\`${err.stack.lengh >= 1024 ? `The full error is too long to show.\n\n${err}` : err.stack}\`\`\``)
-    .addField("Origin", `\`\`\`${origin}\`\`\``)
-    .setTimestamp()
-    .setFooter(`${client.user.username} Error Handling`, client.user.displayAvatarURL({ dynamic: true }))
-    .setColor(`${client.embedColor.noColor}`)
+        .setTitle(`Uncaught Exception`)
+        .setURL("https://nodejs.org/api/process.html#event-uncaughtexception")
+        .addField("Error", `\`\`\`${err.stack.lengh >= 1024 ? `The full error is too long to show.\n\n${err}` : err.stack}\`\`\``)
+        .addField("Origin", `\`\`\`${origin}\`\`\``)
+        .setTimestamp()
+        .setFooter(`${client.user.username} Error Handling`, client.user.displayAvatarURL({ dynamic: true }))
+        .setColor(`${client.embedColor.noColor}`)
 
-    return channel.send({ embeds: [embed]})
+    return channel.send({ embeds: [embed] })
 
 });
 
@@ -46,14 +46,14 @@ process.on("uncaughtExceptionMonitor", (err, origin) => {
     const channel = client.channels.cache.get("901478033628753951")
 
     const embed = new MessageEmbed()
-    .setTitle(`Uncaught Exception Monitor`)
-    .setURL("https://nodejs.org/api/process.html#event-uncaughtexceptionmonitor")
-    .addField("Error", `\`\`\`${err.stack.lengh >= 1024 ? `The full error is too long to show.\n\n${err}` : err.stack}\`\`\``)
-    .addField("Origin", `\`\`\`${origin}\`\`\``)
-    .setTimestamp()
-    .setFooter(`${client.user.username} Error Handling`, client.user.displayAvatarURL({ dynamic: true }))
-    .setColor(`${client.embedColor.noColor}`)
+        .setTitle(`Uncaught Exception Monitor`)
+        .setURL("https://nodejs.org/api/process.html#event-uncaughtexceptionmonitor")
+        .addField("Error", `\`\`\`${err.stack.lengh >= 1024 ? `The full error is too long to show.\n\n${err}` : err.stack}\`\`\``)
+        .addField("Origin", `\`\`\`${origin}\`\`\``)
+        .setTimestamp()
+        .setFooter(`${client.user.username} Error Handling`, client.user.displayAvatarURL({ dynamic: true }))
+        .setColor(`${client.embedColor.noColor}`)
 
-    return channel.send({ embeds: [embed]})
+    return channel.send({ embeds: [embed] })
 
 });
