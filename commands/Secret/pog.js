5907c592aff93e8e4c92337eef4552414cb6a256
NikanWasTaken <nikan1252sabet4801@gmail.com>
2021-12-02 15:53:21 +0330

    main

---
+++ commands/Secret/pog.js
@@ -0,0 +1,26 @@
+const { MessageEmbed, WebhookClient, MessageAttachment, Message } = require('discord.js')
+
+module.exports = {
+    name: 'pog',
+    category: 'Secret',
+    description: 'poggr',
+    cooldown: 2000,
+    userPermissions: ["MANAGE_MESSAGES"],
+
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+
+    run: async (client, message, args) => {
+
+        const array = ["champy", "champ", "pogger", "poggr", "<a:poggr:915939849859653663>", "pogchampy", " pog + champy = <:pogchampy:915940863065096192> "]
+
+
+        const boop = array[~~(Math.random() * array.length)]
+
+        message.channel.send({ content: boop })
+
+    }
+}
