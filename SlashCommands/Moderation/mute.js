bf8bf700257dc68c397e1e0ffe831ea6703ef177
NikanWasTaken <nikan1252sabet4801@gmail.com>
2022-02-01 10:21:22 +0330

    Nikan's Utilities

--- SlashCommands/Moderation/mute.js
+++ SlashCommands/Moderation/mute.js
@@ -1,7 +1,7 @@
 const { Client, MessageEmbed, CommandInteraction } = require("discord.js");
 const ms = require("ms")
 const db = require("../../models/MemberRoles.js")
-require("../../structures/guildMember/mute")
+require("../../structures/GuildMember/mute")
 
 module.exports = {
   name: "mute",
